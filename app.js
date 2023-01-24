fb33bbdef28c2ddf20d0e7153a0cf95e5777a8c3
Nick Wright <nwright@egnyte.com>
2023-01-24 14:55:20 -0500

    added keyboard Array. Added keyboard to keyboard div using keys.forEach()'

--- app.js
+++ app.js
@@ -8,4 +8,48 @@
 
 //create keyboard
 
+// be sure to add flexbox to css
 
+//need array of keyboard values
+
+//create keys using forEach
+
+
+const tile = document.querySelector('.tile')
+const keyboard = document.querySelector('.keyboard')
+
+const keys = [
+    'Q',
+    'W',
+    'E',
+    'R',
+    'T',
+    'Y',
+    'U',
+    'I',
+    'O',
+    'P',
+    'A',
+    'S',
+    'D',
+    'F',
+    'G',
+    'H',
+    'J',
+    'K',
+    'L',
+    'Z',
+    'X',
+    'C',
+    'V',
+    'B',
+    'N',
+    'ENTER',
+    '<<'
+]
+
+keys.forEach(key => {
+    const button = document.createElement('button')
+    button.textContent = key
+    keyboard.append(button)
+})
