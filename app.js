b536832d5de8aebd6f9570b2884bcaf8c68c4ef2
Nick Wright <nwright@egnyte.com>
2023-01-31 16:08:42 -0500

    added keyboard input

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // create word bank array
-const wordBank = ['HAVEN', 'DRAIN', 'SINUS', 'PENNY', 'PASTE', 'BOUGH', 'FOLLY', 'EXERT', 'PARSE', 'ARRAY','RURAL', 'FOUND', 'CRASH', 'REFER', 'TRICK', 'MAFIA', 'STERN', 'SMURF', 'COVER', 'CHIEF', 'BUNCH', 'PILOT', 'QUEST', 'STAMP', 'ROUSE', 'PIZZA', 'JEWEL', 'AGONY']
+const wordBank = ['HAVEN', 'DRAIN', 'SINUS', 'PENNY', 'PASTE', 'BOUGH', 'FOLLY', 'EXERT', 'PARSE', 'ARRAY','RURAL', 'FOUND', 'CRASH', 'RAKES', 'TRICK', 'MAFIA', 'STERN', 'SMURF', 'COVER', 'CHIEF', 'BUNCH', 'PILOT', 'QUEST', 'STAMP', 'ROUSE', 'PIZZA', 'JEWEL', 'AGONY']
 
 // from https://stackoverflow.com/questions/4550505/getting-a-random-value-from-a-javascript-array
 //This code will return a random value from the worBank array listed above by choosing a random value in the range of 0 and the array length
@@ -15,6 +15,13 @@ showWordle()
 const tileDisplay = document.querySelector('.tile-container')
 const keyboard = document.querySelector('.keyboard')
 const messageDisplay = document.querySelector('.messages') 
+
+//starting position using index values of the tileRows array above
+let currentRow = 0;
+let currentTile = 0;
+
+let gameOver = false;
+
 
 //This code sets keys as an array of individual letter values. I want to use them as buttons later
 const keys = [
@@ -57,15 +64,10 @@ const tileRows = [
     ['', '', '', '', '']
 ]
 
-//starting position using index values of the tileRows array above
-let currentRow = 0;
-let currentTile = 0;
-
-let gameOver = false;
 
 
-// This code is from https://developer.mozilla.org/en-US/docs/Web/API/KeyboardLayoutMap/forEach
-//This code takes each array from tileRows above and assigns it a div, which is a cell for a letter in the game. the div is assigned the value of tile and given an ID or tileRow + the index value of the array.
+// This code is from https://developer.mozilla.org/en-US/docs/Web/API/KeyboardLayoutMap/forEach and https://www.youtube.com/watch?v=mpby4HiElek&list=PL7VGP-8h8i1NrD4pgT9XfAk-UiH-yHvP3&index=7&t=177s&ab_channel=CodewithAniaKub%C3%B3w
+//This code takes each array from tileRows above and assigns it a div, which is a cell for a letter in the game. The div is assigned the value of tile and given an ID of tileRow + the index value of the array.
 tileRows.forEach((tileRow, tileRowIndex) => {
     const tile = document.createElement('div')
     tile.setAttribute('id', 'tileRow-' + tileRowIndex)
@@ -79,8 +81,8 @@ tileRows.forEach((tileRow, tileRowIndex) => {
     tileDisplay.append(tile)
 })
 
- // This code is from https://developer.mozilla.org/en-US/docs/Web/API/KeyboardLayoutMap/forEach
-// This code makes each array index a button and gives it an ID of key. Each key is made clickable by the addEventListener. The buttons are then placed in the keyboard div.
+ // This code is from https://developer.mozilla.org/en-US/docs/Web/API/KeyboardLayoutMap/forEach 
+// This code makes each array index a button and gives it an ID of key. Each key is made clickable by the addEventListener. The buttons are then placed in the keyboard div with keyboard.append.
 keys.forEach(key => {
     const button = document.createElement('button')
     button.textContent = key
@@ -88,9 +90,29 @@ keys.forEach(key => {
     button.addEventListener('click', () => click(key))
     keyboard.append(button)
 
+   
+
+})
+//This code is from here: https://www.section.io/engineering-education/keyboard-events-in-javascript/
+//I am adding the capability to use the keyboard to guess words rather than only be allowed to click. I used console.log to get the names and code of the events of a 'keyup, then define them as variables and pass them into my event listener.
+document.addEventListener('keyup', (event)=> {
+    const name = event.key;
+    const code = event.code;
+    const capitalName = name.toUpperCase();
+//     console.log(`Key pressed ${name} \r\n Key code value: ${code}`);
+// }, false);
+    if (code === 'Enter') {
+        checkRows()
+        return
+    }
+    if (code === 'Backspace') {
+        deleteLetter()
+        return
+    }
+    addLetter(capitalName);
 })
 
- //This function checks the enter and backspace keys. If backspace is clicked, the deleteLetter() function is called as a callback. If enter is clicked, checkRows() function is called as a callback.
+ //This function checks the enter and backspace keys. If backspace is clicked, the deleteLetter() function is called as a callback. If enter is clicked, checkRows() function is called as a callback. If neither is clicked, it enters the value of the key clicked.
  const click = (key) => {
     if (key === '<<') {
         deleteLetter()
@@ -126,6 +148,7 @@ const addLetter = (part) => {
     }
  }
 
+ //This code is from https://www.youtube.com/watch?v=mpby4HiElek&list=PL7VGP-8h8i1NrD4pgT9XfAk-UiH-yHvP3&index=7&t=177s&ab_channel=CodewithAniaKub%C3%B3w
  //This function checks the row once all the tiles are filled up. Word is created by join() concatenating each tileRow value that has been entered. The if/else is checking the index values for when to assign certain colors in the addColor() function and which message to show in the showMessage() functions.
  const checkRows = () => {
     const word = tileRows[currentRow].join('')
@@ -153,7 +176,7 @@ const addLetter = (part) => {
     const gameMessage = document.createElement('p')
     gameMessage.textContent = message
     messageDisplay.append(message)
-    setTimeout(() => messageDisplay.remove(message), 5000)
+    // setTimeout(() => messageDisplay.remove(message), 5000)
  }
 
  //this code comes from https://www.youtube.com/watch?v=mpby4HiElek&list=PL7VGP-8h8i1NrD4pgT9XfAk-UiH-yHvP3&index=7&ab_channel=CodewithAniaKub%C3%B3w
