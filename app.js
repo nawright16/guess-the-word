d9a2445cda57fe9c684d2c93f2015c7935ca1c23
Nick Wright <nwright@egnyte.com>
2023-02-03 10:54:27 -0500

    turnd off event listeners. Added styling

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
+
 // create word bank array
 
-const wordBank = ['HAVEN', 'DRAIN', 'SINUS', 'PENNY', 'PASTE', 'BOUGH', 'FOLLY', 'EXERT', 'PARSE', 'ARRAY','RURAL', 'FOUND', 'CRASH', 'RAKES', 'TRICK', 'MAFIA', 'STERN', 'SMURF', 'COVER', 'CHIEF', 'BUNCH', 'PILOT', 'QUEST', 'STAMP', 'ROUSE', 'PIZZA', 'JEWEL', 'AGONY']
+const wordBank = ['HAVEN', 'DRAIN', 'SINUS', 'PENNY', 'PASTE', 'BOUGH', 'FOLLY', 'EXERT', 'PARSE', 'ARRAY', 'RURAL', 'FOUND', 'CRASH', 'RAKES', 'TRICK', 'MAFIA', 'STERN', 'SMURF', 'COVER', 'CHIEF', 'BUNCH', 'PILOT', 'QUEST', 'STAMP', 'ROUSE', 'PIZZA', 'JEWEL', 'AGONY']
 
-// from https://stackoverflow.com/questions/4550505/getting-a-random-value-from-a-javascript-array
+// From https://stackoverflow.com/questions/4550505/getting-a-random-value-from-a-javascript-array
 // This code will return a random value from the worBank array listed above by choosing a random value in the range of 0 and the array length
+// add async function to get word from API
 
-const wordle = wordBank[Math.floor(Math.random() * wordBank.length)]
 
-const showWordle = () => {
-    console.log(wordle)
-}
-showWordle()
+let wordle = wordBank[Math.floor(Math.random() * wordBank.length)]
+
+console.log(wordle)
 
 
 // assign values for each major div
 
 const tileDisplay = document.querySelector('.tile-container')
 const keyboard = document.querySelector('.keyboard')
-const messageDisplay = document.querySelector('.messages') 
+const messageDisplay = document.querySelector('.messages')
 
 // starting position using index values of the tileRows array above
 
@@ -26,6 +26,7 @@ let currentTile = 0;
 
 let gameOver = false;
 
+// let wordle = fetchWord()
 
 // This code sets keys as an array of individual letter values. I want to use them as buttons later
 
@@ -80,10 +81,10 @@ tileRows.forEach((tileRow, tileRowIndex) => {
     tile.setAttribute('id', 'tileRow-' + tileRowIndex)
 
     tileRow.forEach((guess, guessIndex) => {
-       const letter = document.createElement('div')
-       letter.setAttribute('id', 'tileRow-' + tileRowIndex + '-guess-' + guessIndex)
-       letter.classList.add('guess')
-       tile.append(letter)
+        const letter = document.createElement('div')
+        letter.setAttribute('id', 'tileRow-' + tileRowIndex + '-guess-' + guessIndex)
+        letter.classList.add('guess')
+        tile.append(letter)
     })
     tileDisplay.append(tile)
 })
@@ -99,117 +100,129 @@ keys.forEach(key => {
     keyboard.append(button)
 })
 
+
+
+
 // This code is from here: https://www.section.io/engineering-education/keyboard-events-in-javascript/
-// I am adding the capability to use the keyboard to guess words rather than only be allowed to click. I used console.log to get the names and code of the events of a 'keyup, then define them as variables and pass them into my event listener.
+// I am adding the capability to use the keyboard to guess words rather than only be allowed to click. I used console.log to get the names and code of the events of a 'keyup', then define them as variables and pass them into my event listener. Then the code checks if enter or backspace is pressed on the keyboard
 
-document.addEventListener('keyup', (event)=> {
-    const name = event.key;
-    const code = event.code;
-    const capitalName = name.toUpperCase();
+const keyPress = document.addEventListener('keyup', (event) => {
+    if (!gameOver) {
+        const name = event.key;
+        const code = event.code;
+        const capitalName = name.toUpperCase();
 
-//     console.log(`Key pressed ${name} \r\n Key code value: ${code}`);
-// }, false);
-
-    if (code === 'Enter') {
-        checkRows()
-        return
-    } else if (code === 'Backspace') {
-        deleteLetter()
-        return
-    } else {
-    addLetter(capitalName);
+        if (code === 'Enter') {
+            checkRows()
+            return
+        } else if (code === 'Backspace') {
+            deleteLetter()
+            return
+        } else {
+            addLetter(capitalName);
+        }
     }
 })
 
- // This function checks the enter and backspace keys. If backspace is clicked, the deleteLetter() function is called as a callback. If enter is clicked, checkRows() function is called as a callback. If neither is clicked, it enters the value of the key clicked. 
 
- const click = (key) => {
-    if (key === '<<') {
-        deleteLetter()
-        return
-    } else if (key === 'ENTER') {
-        checkRows()
-        return
-    } else {
-    addLetter(key)
+// This function checks the enter and backspace keys. If backspace is clicked, the deleteLetter() function is called as a callback. If enter is clicked, checkRows() function is called as a callback. If neither is clicked, it enters the value of the key clicked. 
+
+const click = (key) => {
+    if (!gameOver) {
+        if (key === '<<') {
+            deleteLetter()
+            return
+        } else if (key === 'ENTER') {
+            checkRows()
+            return
+        } else {
+            addLetter(key)
+        }
     }
- }
 
- // This code adds a letter to the tiles by checking the index values. The function checks the square's row and tile numbers by it's ID assigned in tileRows.forEach above.
+
+
+}
+
+// This code adds a letter to the tiles by checking the index values. The function checks the square's row and tile numbers by it's ID assigned in tileRows.forEach above.
 
 const addLetter = (part) => {
     if (currentTile < 5 && currentRow < 6) {
-    const square = document.getElementById('tileRow-' + currentRow + '-guess-' + currentTile)
-    square.textContent = part;
-    tileRows[currentRow][currentTile] = part;
-    square.setAttribute('data', part);
-    currentTile++;
+        const square = document.getElementById('tileRow-' + currentRow + '-guess-' + currentTile)
+        square.textContent = part;
+        tileRows[currentRow][currentTile] = part;
+        square.setAttribute('data', part);
+        currentTile++;
     }
- }
+}
 
- // This code deletes removes a letter entered in the most recent square and sets it to a blank value
+// This code deletes removes a letter entered in the most recent square and sets it to a blank value
 
- const deleteLetter = () => {
-    if (currentTile > 0 ) {
-    currentTile--
-    const square = document.getElementById('tileRow-' + currentRow + '-guess-' + currentTile)
-    square.textContent = ''
-    tileRows[currentRow][currentTile] = '';
-    square.setAttribute('data', '');
+const deleteLetter = () => {
+    if (currentTile > 0) {
+        currentTile--
+        const square = document.getElementById('tileRow-' + currentRow + '-guess-' + currentTile)
+        square.textContent = ''
+        tileRows[currentRow][currentTile] = '';
+        square.setAttribute('data', '');
     }
- }
+}
 
- // This code is from https://www.youtube.com/watch?v=mpby4HiElek&list=PL7VGP-8h8i1NrD4pgT9XfAk-UiH-yHvP3&index=7&t=177s&ab_channel=CodewithAniaKub%C3%B3w
- // This function checks the row once all the tiles are filled up. Word is created by join() combining each tileRow value that has been entered. The if/else is checking the index values for when to assign certain colors in the addColor() function and which message to show in the showMessage() functions.
+// This code is from https://www.youtube.com/watch?v=mpby4HiElek&list=PL7VGP-8h8i1NrD4pgT9XfAk-UiH-yHvP3&index=7&t=177s&ab_channel=CodewithAniaKub%C3%B3w
+// This function checks the row once all the tiles are filled up. Word is created by join() combining each tileRow value that has been entered. The if/else is checking the index values for when to assign certain colors in the addColor() function and which message to show in the showMessage() functions.
 
- const checkRows = () => {
+const checkRows = () => {
     const word = tileRows[currentRow].join('')
     if (currentTile > 4) {
-       addColor()
+        addColor()
         if (wordle === word) {
             showMessage('Great Job!')
             gameOver = true
             return
-        } else {
-            if (currentRow >= 5) {
-                gameOver = true
-                showMessage('Game Over')
-                return
-            }
-            if (currentRow < 5) {
-                currentRow++
-                currentTile = 0
-            }
+        } else if (currentRow < 5) {
+            currentRow++
+            currentTile = 0
+        } else if (currentRow >= 5) {
+            showMessage('Game Over')
+            gameOver = true
+            return
+
         }
     }
- }
+
+}
 // This function creates gameMessage as a p tag and puts it in the message display div. What message displays depends on the check rows function above
 
- const showMessage = (message) => {
+const showMessage = (message) => {
     const gameMessage = document.createElement('p')
     gameMessage.textContent = message
     messageDisplay.append(message)
-    
- }
 
- // From https://www.youtube.com/watch?v=mpby4HiElek&list=PL7VGP-8h8i1NrD4pgT9XfAk-UiH-yHvP3&index=7&ab_channel=CodewithAniaKub%C3%B3w
+}
 
- const addColor = () => {
-    const rowTiles = document.querySelector('#tileRow-' + currentRow).childNodes   
+// From https://www.youtube.com/watch?v=mpby4HiElek&list=PL7VGP-8h8i1NrD4pgT9XfAk-UiH-yHvP3&index=7&ab_channel=CodewithAniaKub%C3%B3w
+
+const addColor = () => {
+    const rowTiles = document.querySelector('#tileRow-' + currentRow).childNodes
     rowTiles.forEach((tile, index) => {
         const dataLetter = tile.getAttribute('data')
 
-        setTimeout(() => {      
-        if (dataLetter === wordle[index]) {
-            tile.classList.add('green-fillcolor')
-        } else if (wordle.includes(dataLetter)) {
-            tile.classList.add('yellow-fillcolor')
-        } else {
-            tile.classList.add('red-fillcolor')
-        }
-    }, 300 * index)
+        setTimeout(() => {
+            if (dataLetter === wordle[index]) {
+                tile.classList.add('green-fillcolor')
+            } else if (wordle.includes(dataLetter)) {
+                tile.classList.add('yellow-fillcolor')
+            } else {
+                tile.classList.add('red-fillcolor')
+            }
+        }, 300 * index)
     })
- }
+}
 
-// if data === button, and class= red, add class to button
-// if game over,remove event listners
+function isGameOver() {
+    if (gameOver === true) {
+        document.removeEventListener('keyup', checkRows)
+    }
+}
+
+
---
+++ index.js
@@ -0,0 +1,24 @@
+
+// async function fetchWords() {
+//     const options = {
+//         method: 'GET',
+//         headers: {
+//             'X-RapidAPI-Key': '##################################',
+            
+//             'X-RapidAPI-Host': 'random-words5.p.rapidapi.com'
+//         }
+//     }
+//     let response = await fetch('https://random-words5.p.rapidapi.com/getMultipleRandom?count=5&wordLength=5', options)
+//         .then(response => response.json())
+//         .then(response => {console.log(response);
+//             return response})
+//         .catch(err => console.error(err));
+             
+// };
+
+// //let wordBank = 
+// fetchWords().then(response => console.log(response))
+
+//console.log(wordBank)
+
+//https://www.youtube.com/watch?v=cuEtnrL9-H0&ab_channel=WebDevSimplified
